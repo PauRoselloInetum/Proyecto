693696aaf9db2cae21d252ed91e6b3b2e841047e
Rosello-Asens Pau <pau.rosello.ext@inetum.com>
2024-09-12 13:15:59 +0200

    Error control

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatButtonModule, MatFormFieldModule],
+  imports: [MatButtonModule, ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  valid: boolean = true;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+      console.log('Email:', email);
+      console.log('Password:', password);
+    } else {
+      this.valid = false;
+      console.log('Formulario no válido');
+    }
+  }
 }
