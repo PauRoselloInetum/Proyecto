8f097cfe5a6ab593c25d6aebe5a27c29c6b0fac1
Rosello-Asens Pau <pau.rosello.ext@inetum.com>
2024-09-13 14:01:32 +0200

    Loading

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WrongRouteComponent } from './404/404.component';
+import { LoadingComponent } from './loading/loading.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login' , pathMatch: 'full' },
   { path: 'login', component: LoginComponent , pathMatch: 'full' },
+  { path: 'loading', component: LoadingComponent , pathMatch: 'full' },
   // { path: 'login/verify', component: VerificationComponent , pathMatch: 'full' },
   // { path: 'profile', component: PerfilComponent , pathMatch: 'full' },
 
