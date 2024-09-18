219a9e8ab540e1a1745567cc6288ee8fe3587fc7
Rosello-Asens Pau <pau.rosello.ext@inetum.com>
2024-09-18 08:46:19 +0200

    Routes Update

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -12,8 +12,8 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent , pathMatch: 'full' },
   { path: 'register', component: RegisterComponent , pathMatch: 'full' },
   { path: 'loading', component: LoadingComponent , pathMatch: 'full' },
-  { path: 'reset-password', component: ResetPasswordComponent , pathMatch: 'full' },
-  { path: 'forgot-password', component: ForgotPasswordComponent , pathMatch: 'full' },
+  { path: 'login/reset-password', component: ResetPasswordComponent , pathMatch: 'full' },
+  { path: 'login/forgot-password', component: ForgotPasswordComponent , pathMatch: 'full' },
   { path: 'verify', component: VerificationComponent , pathMatch: 'full' },
   // { path: 'profile', component: PerfilComponent , pathMatch: 'full' },
 
