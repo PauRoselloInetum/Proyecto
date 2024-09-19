90522963a8a74fb5615aa43e5922121b59415455
Rosello-Asens Pau <pau.rosello.ext@inetum.com>
2024-09-19 14:00:36 +0200

    Login Error Control v0

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LoadingComponent } from '../loading/loading.component';
 import { CookieService } from 'ngx-cookie-service';
@@ -10,22 +14,36 @@ import { CookieService } from 'ngx-cookie-service';
   standalone: true,
   imports: [FormsModule, HttpClientModule, LoadingComponent],
   templateUrl: './login.component.html',
-  styleUrls: ['../../assets/css/auth.css']
+  styleUrls: ['../../assets/css/auth.css'],
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
   loading: boolean = false;
   error: string = '';
+  info: string = '';
 
+  constructor(
+    private http: HttpClient,
+    private cookieService: CookieService,
+  ) {}
 
-  constructor(private http: HttpClient, private cookieService: CookieService) {}
+  ngOnInit() {
+    if (this.cookieService.get('token')) {
+      this.info = 'Ya estas logeado, redireccionando...';
+      // setTimeout(() =>
+      //   {
+      //     window.location.href = '/';
+      //   },
+      //   3000);
+    }
+  }
 
   login() {
     this.loading = true;
     const loginData = {
       email: this.email,
-      password: this.password
+      password: this.password,
     };
 
     this.postLogin(loginData).subscribe({
@@ -34,11 +52,17 @@ export class LoginComponent {
         this.cookieService.set('token', response);
         this.error = '';
         this.loading = false;
+        this.info = 'Login exitoso';
+        setTimeout(() =>
+          {
+            window.location.href = '/';
+          },
+          3000);
       },
       error: (error) => {
         this.error = error.status;
         this.loading = false;
-      }
+      },
     });
   }
 
@@ -46,9 +70,9 @@ export class LoginComponent {
     const apiUrl = 'https://localhost:7272/api/Controlador/login';
 
     const headers = new HttpHeaders({
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      'Allow-Origin': '*'
+      'Allow-Origin': '*',
     });
 
     return this.http.post(apiUrl, data, { headers });
--- LoginJWT/PruebaDefinitivo/obj/Debug/net8.0/Prueba definitivo.AssemblyInfo.cs
+++ LoginJWT/PruebaDefinitivo/obj/Debug/net8.0/Prueba definitivo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba definitivo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf8f982e824b414792e2547e857fc8640602376")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a25e917e9b666964513fe4ded39b05d21fc2f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba definitivo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba definitivo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
