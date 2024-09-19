388703ff987d20173c1ab206f6258ad6e743a7f2
Flores-Rubias Marta <marta.flores.ext@inetum.com>
2024-09-19 15:29:22 +0200

    Borrar comentario

--- LoginJWT/PruebaDefinitivo/obj/Debug/net8.0/Prueba definitivo.AssemblyInfo.cs
+++ LoginJWT/PruebaDefinitivo/obj/Debug/net8.0/Prueba definitivo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba definitivo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b18717f40a55fd893fb4a69b146a10b7beb2293e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84d4ed2249cce381c5ea2a5c276c8c47b01cbe40")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba definitivo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba definitivo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
