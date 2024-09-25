7718bbe523c7e024e63e2925b5dba5660da06476
Rosello-Asens Pau <pau.rosello.ext@inetum.com>
2024-09-25 08:34:37 +0200

    Login Error Text Fix

--- Angular/src/app/login/login.component.ts
+++ Angular/src/app/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent {
             this.errors += 1;
             this.login()
           }else{
-            this.error = 'Error con Servidor. .Prueba otra vez.';
+            this.error = 'Error con Servidor. Prueba otra vez.';
           }
         }
         this.loading = false;
