18b1c458e722f715df6a1921a38ec5dcda840d88
Flores-Rubias Marta <marta.flores.ext@inetum.com>
2024-09-26 12:12:27 +0200

    Texto de prueba con el email

---
+++ Backend/HireAProBackend/Models/EmailContent.cs
@@ -0,0 +1,9 @@
+﻿namespace HireAProBackend.Models
+{
+    public class EmailContent
+    {
+        public string Welcome { get; set; } = "¡Te damos la bienvenida a Hire a pro!";
+        public string WelcomeBody { get; set; } 
+        public string ChangePassword { get; set; } = "Solicitud de regeneración de contrase&Ntilde;a";
+    }
+}
