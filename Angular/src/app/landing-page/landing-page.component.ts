7b6cfd10e3cccfd36befb9d3391d5ad8f30a5af8
PauRoselloInetum <181321808+PauRoselloInetum@users.noreply.github.com>
2024-10-01 12:32:37 +0200

    Revert "Paum dev"

--- Angular/src/app/landing-page/landing-page.component.ts
+++ Angular/src/app/landing-page/landing-page.component.ts
@@ -2,17 +2,13 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from '../header/header.component';
 
-// Decorador @Component para definir el componente
 @Component({
-  selector: 'app-landing-page', // Definir selector personalizado de HTML para este componente
-  standalone: true, // Marcar este componente como independiente-standalone
-  imports: [HeaderComponent], // Importar el componente secundario para poder usarlo en este componente
-  templateUrl: './landing-page.component.html', // Definir el archivo HTML que contiene la plantilla asociada a este componente
-  styleUrl: './../../assets/css/landing-page.css' // Definir el archivo CSS que contiene los estilos asociados a este componente
+  selector: 'app-landing-page',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './../../assets/css/landing-page.css'
 })
-
-// Componente secundario de la aplicación - Clase vacía sin lógica implementada. 
-// Sólo muestra la vista html de la landing page
 export class LandingPageComponent {
 
 }
--- Angular/src/app/loading/loading.component.ts
+++ Angular/src/app/loading/loading.component.ts
@@ -1,29 +1,19 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 
-// Decorador @Component para definir el componente
 @Component({
-  selector: 'app-loading', // Definir selector personalizado de HTML para este componente
-  standalone: true, // Marcar este componente como independiente-standalone
-  imports: [], // No hay ninguna importación para este componente
-  templateUrl: './loading.component.html', // Definir el archivo HTML que contiene la plantilla asociada a este componente
-  styleUrl: './../../assets/css/loading.css', // Definir el archivo CSS que contiene los estilos asociados a este componente
+  selector: 'app-loading',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading.component.html',
+  styleUrl: './../../assets/css/loading.css',
 })
-
-// Componente secundario de la aplicación
-// Muestra un indicador de carga en la interfaz de usuarip mientras se realizan operaciones asíncronas
 export class LoadingComponent {
-  // PROPIEDADES
   loading: string = '';
 
-  // Constructor vacío para interactuar con AuthService - inyecta el servicio de autenticación que maneja la lógica del loading/carga de la página
-  constructor(private authservice: AuthService){
+  constructor(private authservice: AuthService){}
 
-  }
-
-  // Método ngOnInit - busca los valores de AuthService para actualizar las propiedades locales del componente cuando estos valores cambian
   ngOnInit() {
-    // Actualiza el mensaje de carga
     this.authservice.loading$.subscribe((loading) => {
       this.loading = loading;
     });
