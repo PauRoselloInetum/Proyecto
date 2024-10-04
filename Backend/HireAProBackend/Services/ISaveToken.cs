08d51472a76034c5fd6a574aa74fe3f7dfa46673
Flores-Rubias Marta <marta.flores.ext@inetum.com>
2024-10-04 14:21:11 +0200

    Creado el servicio de guardar tokens en la base de datos SaveToken

---
+++ Backend/HireAProBackend/Services/ISaveToken.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HireAProBackend.Services
+{
+    public interface ISaveToken
+    {
+        Task<bool> GuardarTokenAsync(string tokenEntrada, string correo);
+    }
+}
