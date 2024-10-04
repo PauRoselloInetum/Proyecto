c7d7c753384a0243db72f93d6db4a2cfd10300fb
Flores-Rubias Marta <marta.flores.ext@inetum.com>
2024-10-04 11:21:15 +0200

    Funcion ShaHash como servicio

---
+++ Backend/HireAProBackend/Services/ShaHash.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace HireAProBackend.Services
+{
+    public class ShaHash : IShaHash
+    {
+        public string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2")); // Convertir a hexadecimal
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
