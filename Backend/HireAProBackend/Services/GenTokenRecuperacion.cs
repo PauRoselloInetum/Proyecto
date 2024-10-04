144668cf9dd25fa66a3fa724f2c4c4e71cf1156d
pauManresaInetum <pau.manresa.ext@inetum.com>
2024-10-04 12:12:24 +0200

    Servicios Generación de Token

---
+++ Backend/HireAProBackend/Services/GenTokenRecuperacion.cs
@@ -0,0 +1,34 @@
+﻿namespace HireAProBackend.Services
+{
+    public class GenTokenRecuperacion : IGenTokenRecuperacion
+    {
+
+        // método que va a crear un token personalizado para la url de recuperación de contraseña.
+        // procedimiento --> hash(mail + contra + token perosnalizado)
+        private static readonly Random random = new Random();
+        private const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        private string RandomString(int length)
+        {
+
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public string generarTokenRecuperacion(string mail)
+        {
+
+
+            string bufferToken = mail;
+
+
+            return bufferToken + RandomString(10);
+        }
+
+
+    }
+
+
+
+
+}
